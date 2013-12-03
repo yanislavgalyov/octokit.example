914a0576d224e98bbd18647e832c0c1f828507cc
Yanislav Galyov <y.galyov@gmail.com>
2013-12-03 16:44:00 +0200

    Added ability to export to xls (xml format)

--- Octokit.Example/Program.cs
+++ Octokit.Example/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CarlosAg.ExcelXmlWriter;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
@@ -7,6 +9,19 @@ using System.Threading.Tasks;
 
 namespace Octokit.Example
 {
+    class IssuesH
+    {
+        public List<string> Open { get; set; }
+        public List<string> Closed { get; set; }
+        public string Error { get; set; }
+
+        public IssuesH()
+        {
+            Open = new List<string>();
+            Closed = new List<string>();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -14,13 +29,72 @@ namespace Octokit.Example
             //октомври - 1
             //ноември - 2
             //декември - 3
-            GetIssuesForMilestone("2");
+            var task = GetIssuesForMilestone("2");
+            task.Wait();
+
+            IssuesH result = task.Result;
+
+            if (!string.IsNullOrEmpty(result.Error))
+            {
+                Console.WriteLine(result.Error);
+            }
+            else
+            {
+                ExportToExcel(result);
+            }
 
             Console.Read();
         }
 
-        static async void GetIssuesForMilestone(string milestone)
+        static void ExportToExcel(IssuesH data)
         {
+            try
+            {
+                Workbook book = new Workbook();
+                Worksheet sheet = book.Worksheets.Add("sheet 1");
+                WorksheetStyle style = book.Styles.Add("HeaderStyle");
+                style.Font.Bold = true;
+                style.Alignment.WrapText = true;
+                style = book.Styles.Add("Default");
+                style.Font.FontName = "Tahoma";
+                style.Alignment.WrapText = true;
+                style.Alignment.Horizontal = StyleHorizontalAlignment.Justify;
+                style.Alignment.Vertical = StyleVerticalAlignment.Justify;
+                style.Font.Size = 10;
+
+                WorksheetRow row = sheet.Table.Rows.Add();
+                row.Cells.Add(new WorksheetCell("Отворени:", "HeaderStyle"));
+
+                foreach (var item in data.Open)
+                {
+                    row = sheet.Table.Rows.Add();
+                    row.Cells.Add(item);
+                }
+
+                row = sheet.Table.Rows.Add();
+                row = sheet.Table.Rows.Add();
+                row.Cells.Add(new WorksheetCell("Затворени:", "HeaderStyle"));
+
+                foreach (var item in data.Closed)
+                {
+                    row = sheet.Table.Rows.Add();
+                    row.Cells.Add(item);
+                }
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    book.Save("result.xml");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task<IssuesH> GetIssuesForMilestone(string milestone)
+        {
+            IssuesH result = new IssuesH();
             try
             {
                 string login, pass;
@@ -50,41 +124,46 @@ namespace Octokit.Example
 
                 if (openIssues.Count == 0)
                 {
-                    Console.WriteLine("No open issues");
+                    //Console.WriteLine("No open issues");
                 }
                 else
                 {
-                    Console.WriteLine("Open issues ({0}):", openIssues.Count);
+                    //Console.WriteLine("Open issues ({0}):", openIssues.Count);
                     foreach (var item in openIssues)
                     {
-                        Console.WriteLine(item.Title);
+                        result.Open.Add(item.Title);
+                        //Console.WriteLine(item.Title);
                     }
                 }
 
-                Console.WriteLine("-----------------------");
+                //Console.WriteLine("-----------------------");
 
                 if (closedIssues.Count == 0)
                 {
-                    Console.WriteLine("No open issues");
+                    //Console.WriteLine("No open issues");
                 }
                 else
                 {
-                    Console.WriteLine("Closed issues ({0}):", closedIssues.Count);
+                    //Console.WriteLine("Closed issues ({0}):", closedIssues.Count);
                     foreach (var item in closedIssues)
                     {
-                        Console.WriteLine(item.Title);
+                        result.Closed.Add(item.Title);
+                        //Console.WriteLine(item.Title);
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.Message);
+                result.Error = ex.Message;
             }
             finally
             {
-                Console.WriteLine();
-                Console.WriteLine("Press any key to quit...");
+                //Console.WriteLine();
+                //Console.WriteLine("Press any key to quit...");
             }
+
+            return result;
         }
     }
 }
